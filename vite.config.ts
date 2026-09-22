e07e0c8af7a2e4b89310fa9f19432212af66c597
cindyvanldb <cindy.vanldb@gmail.com>
2026-09-22 20:50:37 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/indochine-chinchu-hotels/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
